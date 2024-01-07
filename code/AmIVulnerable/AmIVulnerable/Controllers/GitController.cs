91ed5ab36c2125e408c6b09cc81d3f2e7d5f6d89
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-01-07 14:36:07 +0100

    Add LibGit2Sharp for Git Support

---
+++ code/AmIVulnerable/AmIVulnerable/Controllers/GitController.cs
@@ -0,0 +1,37 @@
+﻿using LibGit2Sharp;
+using Microsoft.AspNetCore.Mvc;
+using CM = System.Configuration.ConfigurationManager;
+
+namespace AmIVulnerable.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitController : ControllerBase {
+
+        [HttpGet]
+        [Route("clone")]
+        public IActionResult CloneRepo([FromHeader] string url) {
+            try {
+                CM.AppSettings["CloneFinished"] = "false";
+                if (url.Equals("")) {
+                    _ = Clone(CM.AppSettings["StandardCveUrlPlusTag"]!);
+                    return Ok();
+                }
+                else {
+                    _ = Clone(url);
+                    return Ok();
+                }
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private async Task Clone(string url) {
+            await Task.Run(() => {
+                Repository.Clone(url, AppDomain.CurrentDomain.BaseDirectory + "raw");
+                CM.AppSettings["CloneFinished"] = "true";
+            });
+        }
+    }
+}
