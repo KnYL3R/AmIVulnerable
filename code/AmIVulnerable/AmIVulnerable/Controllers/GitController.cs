8394f93d6ccefd1be1657e5b4a8309c8e619bdad
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-01-07 15:28:53 +0100

    Delete raw Folder if already exist

--- code/AmIVulnerable/AmIVulnerable/Controllers/GitController.cs
+++ code/AmIVulnerable/AmIVulnerable/Controllers/GitController.cs
@@ -1,5 +1,7 @@
 ﻿using LibGit2Sharp;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using CM = System.Configuration.ConfigurationManager;
 
 namespace AmIVulnerable.Controllers {
@@ -29,9 +31,37 @@ namespace AmIVulnerable.Controllers {
 
         private async Task Clone(string url){
             await Task.Run(() => {
+                if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "raw")) {
+                    string targetDir = AppDomain.CurrentDomain.BaseDirectory + "raw";
+                    RemoveReadOnlyAttribute(targetDir);
+                    Directory.Delete(targetDir, true);
+                }
                 Repository.Clone(url, AppDomain.CurrentDomain.BaseDirectory + "raw");
                 CM.AppSettings["CloneFinished"] = "true";
             });
+        }
+
+        public void RemoveReadOnlyAttribute(string path) {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            foreach (var file in directoryInfo.GetFiles()) {
+                file.Attributes &= ~FileAttributes.ReadOnly;
+            }
+
+            foreach (var subDirectory in directoryInfo.GetDirectories()) {
+                RemoveReadOnlyAttribute(subDirectory.FullName);
+            }
+        }
+
+        [HttpGet]
+        [Route("cloneStatus")]
+        public IActionResult CloneStatus() {
+            if (CM.AppSettings["CloneFinished"].Equals("true")) {
+                return Ok();
+            }
+            else {
+                return NoContent();
+            }
         }
     }
 }
