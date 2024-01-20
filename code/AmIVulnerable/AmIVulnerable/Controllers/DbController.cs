0ad1c57fb18dd290054c81dbf6bd83e951e4a1fa
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-01-20 15:50:38 +0100

    Single package analysis ready including controller endpoint for LiteDB

--- code/AmIVulnerable/AmIVulnerable/Controllers/DbController.cs
+++ code/AmIVulnerable/AmIVulnerable/Controllers/DbController.cs
@@ -1,5 +1,7 @@
 ﻿using LiteDbLib.Controller;
 using Microsoft.AspNetCore.Mvc;
+using Modells;
+using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 
 namespace AmIVulnerable.Controllers {
@@ -43,7 +45,7 @@ namespace AmIVulnerable.Controllers {
             return Ok();
         }
 
-        private void ExploreFolder (string folderPath, List<string> fileList) {
+        private void ExploreFolder(string folderPath, List<string> fileList) {
             try {
                 fileList.AddRange(Directory.GetFiles(folderPath));
 
@@ -54,6 +56,28 @@ namespace AmIVulnerable.Controllers {
             catch (Exception ex) {
                 Console.WriteLine($"{ex.Message}");
             }
+        }
+
+        [HttpGet, Route("checkSinglePackage")]
+        public IActionResult CheckSinglePackage([FromHeader] string packageName,
+                                                    [FromHeader] bool isDbSearch = true,
+                                                    [FromHeader] string? packageVersion = "") {
+            if (packageVersion.Equals("")) { // search all versions
+                if (isDbSearch) {
+                    SearchDbController searchDbController = new SearchDbController();
+                    List<CveResult> res = searchDbController.SearchSinglePackage(packageName);
+                    if (res.Count > 0) {
+                        return Ok(JsonConvert.SerializeObject(res));
+                    }
+                    else {
+                        return NoContent();
+                    }
+                }
+            }
+            else {
+
+            }
+            return Ok();
         }
     }
 }
