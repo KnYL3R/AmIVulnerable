c6b2ef18c1a02564576f064f0717dec75c57137b
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-02-12 15:40:07 +0100

    Analyze Subtree`s to prepare real analyze

--- code/AmIVulnerable/AmIVulnerable/Controllers/DependeciesController.cs
+++ code/AmIVulnerable/AmIVulnerable/Controllers/DependeciesController.cs
@@ -14,6 +14,7 @@ namespace AmIVulnerable.Controllers {
     public class DependeciesController : ControllerBase {
 
         [HttpGet]
+        [Route("ExtractTree")]
         public IActionResult ExtractDependencies([FromHeader] ProjectType projectType) {
             switch (projectType) {
                 case ProjectType.NodeJs: {
@@ -21,6 +22,23 @@ namespace AmIVulnerable.Controllers {
                         ExecuteCommand("npm", "list --all --json >> tree.json");
                         List<NodePackage> resTree = ExtractTree(AppDomain.CurrentDomain.BaseDirectory + "rawAnalyze/tree.json");
                         F.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "rawAnalyze/depTree.json", JsonConvert.SerializeObject(resTree));
+                        return Ok(JsonConvert.SerializeObject(resTree));
+                    }
+                default: {
+                        return BadRequest();
+                    }
+            }
+        }
+
+        [HttpGet]
+        [Route("ExtractAndAnalyzeTree")]
+        public IActionResult ExtractAndAnalyzeTree([FromHeader] ProjectType projectType) {
+            switch (projectType) {
+                case ProjectType.NodeJs: {
+                        ExecuteCommand("npm", "install");
+                        ExecuteCommand("npm", "list --all --json >> tree.json");
+                        List<NodePackage> depTree = ExtractTree(AppDomain.CurrentDomain.BaseDirectory + "rawAnalyze/tree.json");
+                        List<NodePackage> resTree = analyzeTree(depTree);
                         return Ok(JsonConvert.SerializeObject(resTree));
                     }
                 default: {
@@ -73,6 +91,29 @@ namespace AmIVulnerable.Controllers {
             }
 
             return nodePackage;
+        }
+
+        private List<NodePackage> analyzeTree(List<NodePackage> depTree) {
+            List<Tuple<string, string>> nodePackages = [];
+            foreach (NodePackage x in depTree) {
+                List<NodePackage> y = analyzeSubtree(x);
+                foreach (NodePackage z in y) {
+                    Tuple<string, string> tuple = new Tuple<string, string>(z.Name, z.Version);
+                    if (!nodePackages.Contains(tuple)) {
+                        nodePackages.Add(tuple);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private List<NodePackage> analyzeSubtree(NodePackage nodePackage) {
+            List<NodePackage> res = [];
+            foreach(NodePackage x in nodePackage.Dependencies) {
+                res.AddRange(analyzeSubtree(x));
+            }
+            res.Add(nodePackage);
+            return res;
         }
     }
 }
