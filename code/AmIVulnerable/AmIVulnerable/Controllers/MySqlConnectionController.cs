37cb832fa16bda1cf42b8bcecc821c224db7abf2
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-03-13 19:41:39 +0100

    Demo Controller for MySql Connection to intern db
    https://localhost:8443/api/MySqlConnection/checkReachable
    Link for check the reachable

---
+++ code/AmIVulnerable/AmIVulnerable/Controllers/MySqlConnectionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using SerilogTimings;
+using System.Data;
+
+namespace AmIVulnerable.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MySqlConnectionController : ControllerBase {
+
+        private readonly IConfiguration Configuration;
+
+        public MySqlConnectionController(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        [HttpGet, Route("checkReachable")]
+        public IActionResult PingWithDb() {
+            using (Operation.Time("TaskDuration")) {
+                try {
+                    MySqlConnection c = new MySqlConnection(Configuration["ConnectionStrings:cvedb"]);
+
+                    MySqlCommand cmd = new MySqlCommand("SELECT * FROM cve", c);
+
+                    c.Open();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    reader.Close();
+                    c.Close();
+
+                    string r = "";
+                    foreach (DataRow row in dataTable.Rows) {
+                        foreach (object? item in row.ItemArray) {
+                            r += item;
+                        }
+                    }
+
+                    return Ok(r);
+                }
+                catch (Exception ex) {
+                    return BadRequest(ex.ToString());
+                }
+            }
+        }
+    }
+}
