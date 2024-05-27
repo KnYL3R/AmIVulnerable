a71d4bdf8081decba7bebc163b4474db2f048c41
KnYL3R <theknifeoftruthde@gmail.com>
2024-05-27 20:39:50 +0200

    added sql for table

---
+++ code/AmIVulnerable/Modells/ReportMetric.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Modells {
+    
+    public class ReportMetric {
+
+        /// <summary></summary>
+        [JsonPropertyName(nameof(TotalDirectVulnerabilities))]
+        public int TotalDirectVulnerabilities { get; set; } = new int();
+
+        /// <summary></summary>
+        [JsonPropertyName(nameof(TotalTransitiveVulnerabilities))]
+        public int TotalTransitiveVulnerabilities { get; set; } = new int();
+
+        /// <summary></summary>
+        [JsonPropertyName(nameof(MeanSeverityDirectVulnerabilities))]
+        public int MeanSeverityDirectVulnerabilities { get; set; } = new int();
+
+        /// <summary></summary>
+        [JsonPropertyName(nameof(MeanSeverityTransitiveVulnerabilities))]
+        public int MeanSeverityTransitiveVulnerabilities { get; set; } = new int();
+    }
+}
